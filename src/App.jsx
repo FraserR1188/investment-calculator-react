f40dd1a84d4e8e814125070b2c84127d15f33c70
Robbie Fraser <fraserrobbie2@gmail.com>
2026-02-22 16:02:08 +0000

    intro / add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescription = ["Fundamental", "Crucial", "Important"];
 
@@ -21,11 +22,38 @@ function Header() {
   );
 }
 
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.imgAlt} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="Components let you split the UI into independent, reusable pieces, and think about each piece in isolation."
+              image={componentsImg}
+              imgAlt="Components"
+            />
+            <CoreConcepts
+              title="Props"
+              description="Props (short for properties) are a way to pass data from parent to child components."
+            />
+            <CoreConcepts />
+            <CoreConcepts />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
