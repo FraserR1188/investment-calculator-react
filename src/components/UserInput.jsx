9d6770770857e9bf33dbf7c2398501dcc21465fd
Robbie Fraser <fraserrobbie2@gmail.com>
2026-04-10 16:17:52 +0100

    add onChange within the input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -8,12 +8,27 @@ export default function UserInput() {
     duration: 10,
   });
 
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="">Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(e) =>
+              handleChange("initialInvestment", parseFloat(e.target.value))
+            }
+          />
         </p>
         <p>
           <label htmlFor="">Annual Investment</label>
